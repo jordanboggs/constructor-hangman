93de4eea46279399b54e53612b1c453abb80ab1e
Jordan Boggs <boggsjp@gmail.com>
2018-01-25 17:35:59 -0700

    Now we know if an answer is right or wrong

--- Word.js
+++ Word.js
@@ -3,14 +3,32 @@ const Letter = require("./Letter.js");
 const Word = function(word) {
   this.word = word;
   this.array = this.objectifyLetters(word);
+  this.arrayOfChars = this.makeArrayOfChars(this.array);
+  this.arrayOfBlanks = this.makeArrayOfBlanks(this.array);
 };
 
 Word.prototype.objectifyLetters = function(word) {
-  let array = word.split("");
-  array.forEach(function(letter, index) {
-    array[index] = new Letter(letter);
+  let arr = word.split("");
+  arr.forEach(function(letter, index) {
+    arr[index] = new Letter(letter);
   });
-  return array;
+  return arr;
+};
+
+Word.prototype.makeArrayOfChars = function(objectArray) {
+  let arr = [];
+  for (let i = 0; i < objectArray.length; i++) {
+    arr[i] = objectArray[i].guessed;
+  }
+  return arr;
+};
+
+Word.prototype.makeArrayOfBlanks = function(objectArray) {
+  let arr = [];
+  for (let i = 0; i < objectArray.length; i++) {
+    arr[i] = objectArray[i].blank;
+  }
+  return arr;
 };
 
 module.exports = Word;
--- hangman.js
+++ hangman.js
@@ -24,19 +24,56 @@ const pickWord = function() {
 };
 
 let newWord = pickWord();
+let guessedArray = newWord.arrayOfChars;
+let blanksArray = newWord.arrayOfBlanks;
 
+// DELETE later, this is for testing only
+console.log(colors.blue("The word is " + newWord.word));
+console.log(blanksArray);
+console.log(colors.grey(blanksArray.join(" ")));
+
+// Set up the prompt
 let schema = {
   properties: {
     letter: {
-      description: colors.magenta("Guess a letter")
+      description: colors.magenta("Guess a letter:")
     }
   }
 };
-
-prompt.message = colors.red("?");
+// Set up colors
+prompt.message = colors.cyan("?");
 prompt.delimiter = " ";
-
+// Begin prompt
 prompt.start();
 prompt.get(schema, function(err, result){
-  console.log(colors.cyan(result.letter));
+  // console.log(colors.cyan(result.letter));
+  if (err) {
+    console.log(colors.red(err));
+  }
+  else {
+    // Program will reject all letters after initial letter
+    let letter = result.letter[0];
+
+    // Program will detect if the guess was wrong
+    let success = 0;
+
+    // Check array for matches
+    for (let i = 0; i < guessedArray.length; i++) {
+      if (letter === guessedArray[i]) {
+        // Replace the space in blanksArray
+        blanksArray[i] = letter;
+        success++;
+      }
+    }
+
+    // Check if right or wrong
+    if (success === 0) {
+      guesses--;
+      console.log(colors.red("Incorrect!"));
+      console.log("Guesses left: " + guesses);
+    }
+    else {
+      console.log(colors.green(blanksArray.join(" ")));
+    }
+  }
 });
